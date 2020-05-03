84b6484230d2bc149d0ef736eb8abd4084be3472
dsajwan98 <37627908+dsajwan98@users.noreply.github.com>
2020-05-04 01:58:05 +0530

    Initial Testing

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,25 @@
 pragma solidity >=0.4.21 <0.7.0;
 contract Election{
-    string public candidate;
+    //Model a Candidate
+    struct Candidate{
+        uint id;
+        string name;
+        uint voteCount;
+    }
+    //Store Candidate
+    //Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    //Store Candidate Count
+    uint public candidatesCount;
 
     constructor() public{
-        candidate = "candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private{
+        candidatesCount  ++;
+        candidates[candidatesCount] = Candidate(candidatesCount,_name,0);
     }
 
 }
